e779ee1de3e25f0afee5d1fdc1a0360bd8deed90
Gusain, Nidhi <NidhiGusain@quickenloans.com>
2021-10-05 14:55:29 -0400

    initial commit

---
+++ JavaScriptLab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JavaScriptLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
